b6e664bacfd735287d1ebbdf35ea79cc8e4376ec
Youtube608 <prahladchoudhary2010@gmail.com>
2024-06-28 08:25:16 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var addToCartButtons = document.querySelectorAll('.add-to-cart');
-
-    addToCartButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
-            alert('Added to cart!');
-        });
-    });
-});
